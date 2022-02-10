e8c2971d33d4cc65ccbebc545e94a5ae3f8ac8c3
KecoaBerotak <dennisfernandes0212@gmail.com>
2022-02-10 23:23:50 +0700

    banyak

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,44 +1,82 @@
 import React, { useState } from "react";
-import AddNewTodo from "./AddNewTodo";
 
 const TodoList = () => {
-    const [todos, setTodos] = useState([
-        {text: 'Makan', id: 1},
-        {text: 'Tidur', id: 2},
-        {text: 'Main', id: 3}
-    ])
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("");
+    const [editId, setEditId] = useState(null);
+    const [editText, setEditText] = useState("");
 
-    const addTodo = (isi) => {
-        setTodos([
-            ...todos,
-            {text:isi, id: Math.random()}
-        ]);
+    const addTodo = (e) => {
+        e.preventDefault();
+
+        const newTodo = {
+            id: new Date().getTime(),
+            text: todo,
+            status: false
+        }
+
+        setTodos([...todos].concat(newTodo));
+        setTodo('');
     }
 
-    const removeTodo = (todoId) => {
-        const filterTodo = todos.filter(todo => {
+    const deleteTodo = (todoId) => {
+        const filterTodo = [...todos].filter((todo) => {
             return todo.id !== todoId;
         });
-
+        console.log('delete')
         setTodos(filterTodo);
+    }
+
+    const statusTodo = (todoId) => {
+        const status = [...todos].map(todo => {
+            if(todo.id === todoId){
+                todo.status = !todo.status;
+            }
+            return todo;
+        });
+        setTodos(status);
+        console.log(todos);
+    }
+
+    const editTodo = (todoId) => {
+        const updateTodo = [...todos].map((todo) => {
+            if(todo.id === todoId){
+                todo.text = editText;
+            }
+            return todo;
+        });
+
+        setTodos(updateTodo);
+        setEditId(null);
+        setEditText("");
     }
 
     return (
         <div>
-            <h1>Todo List</h1>
-            <ul>
-                {todos.map(todo => {
-                    return (
-                        <li key={todo.id}>
-                            <p>{todo.text}</p>
-                            <button onClick={removeTodo.bind(this,todo.id)}>Hapus</button>
-                        </li>
-                    )
-                })}
-            </ul>
-            <AddNewTodo newTodo = {addTodo}/>
+            <h1>TODO LIST</h1>
+            {todos.map((todo) => 
+            <ul key={todo.id}>
+                <li>
+                    {editId === todo.id ? 
+                        (<input type="text"  onChange={(e) => setEditText(e.target.value)} value={editText}/>) 
+                        : 
+                        (<>{todo.text}</>)}
+                    <button onClick={deleteTodo.bind(this,todo.id)}>Delete</button>
+                    {editId === todo.id ? 
+                        (<button onClick={editTodo.bind(this,todo.id)}>Submit Edit</button>) 
+                        : 
+                        (<button onClick={()=>setEditId(todo.id)}>Edit</button>)}
+                    <input type="checkbox" onChange={statusTodo.bind(this,todo.id)} checked={todo.status}/>
+                    {console.log(editId)}
+                </li>
+            </ul>)}
+            <form onSubmit={addTodo}>
+                <input type="text" value={todo} onChange={(e) => {setTodo(e.target.value)}}/>
+                <button>Add Todo</button>
+            </form>
         </div>
     )
+
 }
 
 
